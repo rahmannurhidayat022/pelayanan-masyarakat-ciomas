a3a4a1278599f8956e2ac10d8164072662f3f0ca
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2023-07-04 20:41:45 +0700

    improved landing ui

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -74,7 +74,7 @@
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
                             <h5 class="text-white text-uppercase mb-3 animated slideInDown">Desa Ciomas</h5>
-                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Mahayuna Kadatuan</h1>
+                            <h1 class="display-4 text-white mb-md-4 animated zoomIn">Mahayuna Ayuna Kadatuan</h1>
                             <a href="quote.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Pengajuan Surat</a>
                             <a href="" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Pengaduan</a>
                         </div>
@@ -84,10 +84,10 @@
                     <img class="w-100" src="{{ asset('assets/images/carousel-2.jpg') }}" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
-                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5>
-                            <h1 class="display-1 text-white mb-md-4 animated zoomIn">Creative & Innovative Digital Solution</h1>
-                            <a href="quote.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Free Quote</a>
-                            <a href="" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Contact Us</a>
+                            <h5 class="text-white text-uppercase mb-3 animated slideInDown">Desa Ciomas</h5>
+                            <h1 class="display-4 text-white mb-md-4 animated zoomIn">Menghadapi Pembangunan Kebahagiaan Daerah</h1>
+                            <a href="quote.html" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Pengajuan Surat</a>
+                            <a href="" class="btn btn-outline-light py-md-3 px-md-5 animated slideInRight">Pengaduan</a>
                         </div>
                     </div>
                 </div>
